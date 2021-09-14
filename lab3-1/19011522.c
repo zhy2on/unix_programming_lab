8025a35c97ae6edee9f28589bce25551d9e39e14
zhy2on <dhwlgus5940@gmail.com>
2021-09-15 02:27:45 +0900

    lab 3-1

---
+++ lab3-1/19011522.c
@@ -0,0 +1,34 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void) {
+	int fd, i;
+	
+	//(a)
+	fd = open("data1", O_RDWR|O_CREAT|O_TRUNC, 0600);
+	for (i = 0; i < 10; i++)
+		write(fd, "X", 1);
+	//(b)
+	lseek(fd, 0, SEEK_SET);
+	write(fd, "Y", 1);
+	//(c)
+	lseek(fd, 0, SEEK_END);
+	write(fd, "Y", 1);
+	//(d)
+	for (i = 1; i < 9; i += 2) {
+		lseek(fd, i, SEEK_SET);
+		write(fd, "Z", 1);
+	}
+	//(e)
+	lseek(fd, 14, SEEK_SET);
+	write(fd, "T", 1);
+	//(f)
+	lseek(fd, -2, SEEK_END);
+	write(fd, "S", 1);
+	//(g)
+	lseek(fd, -2, SEEK_CUR);
+	write(fd, "W", 1);
+
+	return (0);
+}
