f8db9c8905fbcf6276f92c2c42edec5b1c23a9f4
zhy2on <dhwlgus5940@gmail.com>
2021-09-28 04:39:29 +0900

    lab5-1

---
+++ lab5-1/19011522.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <ftw.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+int list(const char *name, const struct stat *status, int type) {
+
+	if (type == FTW_D){
+		printf("%s : %d\n", name, status->st_size);
+	}
+	else if (type == FTW_F){
+		if (S_IXUSR & status->st_mode || S_IXGRP & status->st_mode ||
+				 S_IXOTH & status->st_mode)
+			printf("%s : %d\n", name, status->st_size);
+	}
+	return 0;
+}
+int main(void) {
+
+	ftw(".", list, 1);
+
+	return 0;
+}
