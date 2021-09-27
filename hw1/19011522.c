f6aa44eeb122c7406596cbde56686f15fec43c65
zhy2on <dhwlgus5940@gmail.com>
2021-09-28 07:27:52 +0900

    hw2 - ver2

--- hw1/19011522.c
+++ hw1/19011522.c
@@ -33,7 +33,7 @@ void cp_m(char **res){
 	int fd1, fd2, n;
 
 	if ((fd1 = open(res[1], O_RDONLY)) > 0) {
-		fd2 = open(res[2], O_CREAT|O_WRONLY|O_TRUNC, 0600);
+		fd2 = open(res[2], O_WRONLY|O_CREAT|O_TRUNC, 0600);
 		while ((n = read(fd1, buf, sizeof(buf))) > 0)
 			write(fd2, buf, n);
 	}
@@ -41,31 +41,25 @@ void cp_m(char **res){
 void mkdir_m(char **res){
 	mkdir(res[1], 0700);
 }
-int list(const char *name, const struct stat *buf, int type) {
-	if (strcmp(name, ".") == 0)
-		return 0;
-	printf("%c %04o %ld %ld %ld %ld %s %s\n", type == FTW_F ? 'F' : 'D', buf->st_mode&0777, buf->st_nlink, buf->st_uid, buf->st_gid, buf->st_size, strtok(ctime(&buf->st_mtime), "\n"), name + 2);
-
-	return 0;
-}
 void ls_m(char **res){
-	//ftw(".", list, 1);
 	DIR *dp;
 	struct dirent *d;
 	struct stat buf;
 
 	dp = opendir(".");
 	while (d = readdir(dp)) {
+		if (d->d_name[0] == '.')
+			continue;
 		stat(d->d_name, &buf);
 		printf("%c %04o %ld %ld %ld %ld %s %s\n", S_ISDIR(buf.st_mode) ? 'D' : 'F', buf.st_mode&0777, buf.st_nlink, buf.st_uid, buf.st_gid, buf.st_size, strtok(ctime(&buf.st_mtime), "\n"), d->d_name);
 	}
 }
 int main(void){
+	char path[100] = {0};
 	char in[50], *res[20]={0};
 	char *inst[5]={"cat", "cd", "cp", "mkdir", "ls"};
 	void (*f[5])(char **)={cat_m, cd_m, cp_m, mkdir_m, ls_m};
 	int i;
-	char path[100] = {0};
 
 	while (1){
 		printf("%s> ", getcwd(path, 99));
