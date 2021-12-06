97cdf33601b48136e10b42b400555743f3026636
zhy2on <dhwlgus5940@gmail.com>
2021-12-07 03:34:34 +0900

    lab13-1

--- lab13-1/19011522-A.c
+++ lab13-1/19011522-A.c
@@ -35,17 +35,18 @@ int main(void){
 	union semun arg;
 	struct sembuf p_buf;
 
-	key=ftok("key", 5);
-	semid = semget(key, 1, 0600|IPC_CREAT|IPC_EXCL); // semaphore 생성
+	key=ftok("key", 10);
+	semid = semget(key, 2, 0600|IPC_CREAT|IPC_EXCL); // semaphore 생성
 	if (semid==-1){
 		// semaphore get
-		semid = semget(key, 1, 0);			
+		semid = semget(key, 2, 0);			
 	}
 	else{
 		// semaphore 값 초기화
-		arg.val = 1;
-		semctl(semid, 0, SETVAL, arg);
+		arg.array = buf;
+		semctl(semid, 0, SETALL, arg);
 	}
+	printf("semid: %d\n", semid);
 	pid=getpid();
 	for (i=0; i<3; i++){ 
 		// 동기화를 위한 semaphore 연산
@@ -53,10 +54,10 @@ int main(void){
 		semop(semid, &p_buf, 1);
 		for (j=0; j<3; j++){
 			printf("%d ... %ld\n", i, pid);
-			sleep(1);
+			sleep(3);
 		}
 		// 동기화를 위한 semaphore 연산
-		set_sembuf(&p_buf, 0, 1);
+		set_sembuf(&p_buf, 1, -1);
 		semop(semid, &p_buf, 1);
 	}
 	return 0;
--- lab13-1/19011522-B.c
+++ lab13-1/19011522-B.c
@@ -29,28 +29,28 @@ void set_sembuf(struct sembuf *p_buf, int snum, int sop) {
 }
 
 int main(void){
-	ushort buf[2] = {1, 1};
+	ushort buf[2] = {0, 0};
 	int i, j, pid, semid;
 	key_t key;
 	union semun arg;
 	struct sembuf p_buf;
 
-	key=ftok("key", 5);
-	semid = semget(key, 1, 0600|IPC_CREAT|IPC_EXCL);// semaphore 생성
+	key=ftok("key", 10);
+	semid = semget(key, 2, 0600|IPC_CREAT|IPC_EXCL);// semaphore 생성
 	if (semid==-1){
 		// semaphore get
-		semid = semget(key, 1, 0);
+		semid = semget(key, 2, 0);
 	}
 	else{
 		// semaphore 값 초기화
-		arg.val = 1;
-		semctl(semid, 0, SETVAL, arg);
-
+		arg.array = buf;
+		semctl(semid, 0, SETALL, arg);
 	}
+	printf("semid: %d\n", semid);
 	pid=getpid();
 	for (i=0; i<3; i++){ 
 		// 동기화를 위한 semaphore 연산
-		set_sembuf(&p_buf, 0, -1);
+		set_sembuf(&p_buf, 1, 1);
 		semop(semid, &p_buf, 1);
 		for (j=0; j<3; j++){
 			printf("%d ... %ld\n", i, pid);
