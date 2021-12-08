5f4fcbc97bac66e1fab7721bf76f917a7c57093f
zhy2on <dhwlgus5940@gmail.com>
2021-12-08 21:29:20 +0900

    lab12

--- lab12/19011522-1-S.c
+++ lab12/19011522-1-S.c
@@ -20,6 +20,7 @@ int main(void) {
 	char f[6][3]={"f1", "f2", "f3", "f4", "f5", "f6"}; // 6개의 fifo 이름 정의
 	char buf[512];
 	int i, fd[6], n;
+	int tmp;
 	fd_set set, master;
 	int time;
 	struct timeval t;
@@ -28,6 +29,7 @@ int main(void) {
 		// 6개의 fifo 만들고, open
 		mkfifo(f[i], 0600);
 		fd[i] = open(f[i], O_RDWR);
+		//fd[i] = open(f[i], O_RDONLY); // fifo open을 RDONLY, WRONLY로 하게 되면 상대가 fifo open을 할 때까지 blocking.
 	}
 	
 	// select 문을 위한 master값 설정
@@ -38,7 +40,8 @@ int main(void) {
 	// select 문의 timer 값 설정
 	time = 30;
 	t.tv_sec = time;
-	while (set=master, select(fd[2]+1, &set, NULL, NULL, &t)>0){
+	while (set=master, (tmp=select(fd[2]+1, &set, NULL, NULL, &t))>0){
+		printf("tmp: %d\n", tmp);
 		for (i=0; i<3; i++){
 			if (FD_ISSET(fd[i], &set)){
 				if (read(fd[i], buf, sizeof(buf)) > 0){
@@ -46,6 +49,9 @@ int main(void) {
 					n=strlen(buf);
 					// 문자열의 길이 보내기
 					write(fd[i+3], &n, sizeof(int));
+				}
+				else {
+					printf("...\n");
 				}
 			}
 		}
